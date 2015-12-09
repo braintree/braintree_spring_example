81252ecd8deb0a42de69d6935ac9550ccac8af0a
jdlc and rod <code@getbraintree.com>
2015-12-09 21:28:18 +0000

    Adhere to RESTful conventions.

--- src/main/java/service/CheckoutController.java
+++ src/main/java/service/CheckoutController.java
@@ -1,23 +1,63 @@
 package service;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Value;
+import java.math.BigDecimal;
+
+import com.braintreegateway.BraintreeGateway;
+import com.braintreegateway.Result;
+import com.braintreegateway.Transaction;
+import com.braintreegateway.TransactionRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import com.braintreegateway.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class CheckoutController {
 
-    BraintreeConfiguration config = new BraintreeConfiguration();
+    private BraintreeConfiguration config = new BraintreeConfiguration();
+    private BraintreeGateway gateway = config.gateway();
 
-    @RequestMapping("/checkout")
+    @RequestMapping(value = "/checkouts", method = RequestMethod.GET)
     public String checkout(Model model) {
-        BraintreeGateway gateway = config.gateway();
         String clientToken = gateway.clientToken().generate();
         model.addAttribute("clientToken", clientToken);
-        return "checkout";
+
+        return "checkouts/new";
+    }
+
+    @RequestMapping(value = "/checkouts", method = RequestMethod.POST)
+    public String postForm(@RequestParam("amount") String amount, @RequestParam("payment_method_nonce") String nonce,  Model model, final RedirectAttributes redirectAttributes) {
+        TransactionRequest request = new TransactionRequest()
+            .amount(new BigDecimal(amount))
+            .paymentMethodNonce(nonce)
+            .options()
+                .submitForSettlement(true)
+                .done();
+
+        Result<Transaction> result = gateway.transaction().sale(request);
+        if (result.isSuccess()) {
+            Transaction transaction = result.getTarget();
+            return "redirect:checkouts/" + transaction.getId();
+        } else {
+            redirectAttributes.addFlashAttribute("message", "Transaction failed!");
+            return "redirect:checkouts";
+        }
+    }
+
+    @RequestMapping(value = "/checkouts/{transactionId}")
+    public String getTransaction(@PathVariable String transactionId, Model model) {
+        Transaction transaction;
+        try {
+            transaction = gateway.transaction().find(transactionId);
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+            return "redirect:/checkouts";
+        }
+        model.addAttribute("transaction", transaction);
+        return "checkouts/show";
     }
 }
--- src/test/java/service/CheckoutControllerTest.java
+++ src/test/java/service/CheckoutControllerTest.java
@@ -1,35 +1,29 @@
 package service;
 
+import java.math.BigDecimal;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.mock.http.MockHttpOutputMessage;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+import com.braintreegateway.BraintreeGateway;
+import com.braintreegateway.Result;
+import com.braintreegateway.Transaction;
+import com.braintreegateway.TransactionRequest;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
@@ -47,18 +41,23 @@ public class CheckoutControllerTest {
     }
 
     @Test
-    public void returnsOK() throws Exception {
-        mockMvc.perform(get("/checkout"))
+    public void checkoutReturnsOK() throws Exception {
+        mockMvc.perform(get("/checkouts"))
             .andExpect(status().isOk());
     }
 
     @Test
-    public void rendersView() throws Exception {
-        mockMvc.perform(get("/checkout"))
-            .andExpect(view().name("checkout"))
+    public void rendersNewView() throws Exception {
+        mockMvc.perform(get("/checkouts"))
+            .andExpect(view().name("checkouts/new"))
             .andExpect(model().hasNoErrors())
             .andExpect(model().attributeExists("clientToken"))
             .andExpect(xpath("//script[@src='https://js.braintreegateway.com/v2/braintree.js']").exists());
+    }
 
+    @Test
+    public void redirectsOnTransactionNotFound() throws Exception {
+        mockMvc.perform(post("/checkouts/invalid-transaction"))
+            .andExpect(status().isFound());
     }
 }
